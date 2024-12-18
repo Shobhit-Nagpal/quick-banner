b6c2e518d7da9777d9d11590bbf564acabce2a3a
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-18 23:57:35 +0530

    add: navbar + basic canvas

---
+++ src/components/canvas.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+
+type CanvasOpts = {};
+
+type CanvasProps = {
+  opts: CanvasOpts;
+  bgColor: string;
+};
+
+export function Canvas({ opts, bgColor = "#000000" }: CanvasProps) {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    let animationFrameId = 0;
+
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext("2d");
+
+      if (!context) {
+        throw new Error("Context is null");
+      }
+
+      context.fillStyle = bgColor;
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+      const render = () => {
+        context.fillStyle = bgColor;
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        animationFrameId = window.requestAnimationFrame(render);
+      };
+
+      render();
+
+      return () => {
+        window.cancelAnimationFrame(animationFrameId);
+      };
+    }
+  }, [bgColor]);
+
+  return (
+    <canvas ref={canvasRef} className="w-full h-full border border-green-500">
+      Quick Banner Canvas
+    </canvas>
+  );
+}
---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Navbar() {
+  return (
+    <section className="w-full flex items-center justify-between max-w-lg px-6 py-4">
+      <div>Quick Banner</div>
+    </section>
+  );
+}
